7a4ebd90259047860cad6e5651e1896d24aeaa1a
whirlvoid <tomskikh.denis@gmail.com>
2025-08-11 13:33:59 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+import { useState, useEffect, useRef } from "react";
+import vibr from '/assets/vibr.png'
+import sound from '/assets/audio1.mp3'
+
+const MovingElement = () => {
+  const audioRef = useRef(new Audio(sound)); // Сохраняем Audio в useRef
+  const [isMoving, setIsMoving] = useState(false);
+  const [isPeter, setIsPeter] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const audio = audioRef.current; // Берём текущий Audio
+    if (!isMoving){
+        audio.pause();
+      audio.currentTime = 0; // Сбрасываем время трека (опционально)
+        return
+    } // Если анимация выключена, ничего не делаем
+    audio.play()
+    let frameId: any;
+    let direction = 1; // 1 - вправо, -1 - влево
+    let verticalDirection = 1; // 1 - вниз, -1 - вверх
+    let steps = 0;
+
+    const animate = () => {
+      setPosition((prev) => {
+        // Меняем направление каждые 50 шагов
+        if (steps % 1 === 0) {
+          direction = Math.random() > 0.5 ? 1 : -1; // Меняем горизонтальное направление
+          verticalDirection = Math.random() > 0.5 ? 1 : -1; // Случайное вертикальное направление
+        }
+
+        return {
+          x: prev.x + direction * 4, // Движение по X
+          y: prev.y + verticalDirection * 2, // Случайное движение по Y
+        };
+      });
+
+      steps++;
+      frameId = requestAnimationFrame(animate);
+    };
+
+    animate();
+
+    return () => {
+      if (frameId) cancelAnimationFrame(frameId); // Очистка анимации
+    };
+  }, [isMoving]); // Зависимость от isMoving
+
+  return (
+    <div className={isPeter ? 'peter': 'nopiter'}>
+      <div className='main'>
+       <div
+        style={{
+          transform: `translate(${position.x}px, ${position.y}px)`,
+          transition: "transform 0.1s ease-out", // Плавное движение
+        }}
+      ><img src={vibr} className="image"></img></div>
+     <button onClick={() => setIsMoving(!isMoving)} className="btn">
+        {isMoving ? "Устал" : "Пошло поехало"}
+      </button>
+         <button onClick={() => setIsPeter(!isPeter)} className="btn2">
+        {isPeter ? "Стандарт эдишн" : "Петербург эдишн"}
+    </button>
+        <div>
+            розовый х@й — садись и кайфуй™
+        </div>
+    </div>
+
+
+    </div>
+
+  );
+};
+
+export default MovingElement;
